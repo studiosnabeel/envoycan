7b1edb502d935057c05df37dce412f06f366d48f
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-21 15:01:18 +0500

    Adding routes and nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/register/Register';
 import SignIn from './pages/signin/SignIn';
-import Home from './components/home/Home.jsx';
+import Home from './components/home/Home';
+import SideMenu from './components/sidemenu/SideMenu';
+import PageContent from './components/pagecontent/PageContent';
+import Dashboard from './components/dashboard/Dashboard';
+import Billing from './components/billing/Billing';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />}>
+          <Route path="/sidemenu" element={<SideMenu />} />
+          <Route path="/pagecontent" element={<PageContent />}>
+            <Route index element={<Dashboard />} />
+            <Route path="/pagecontent/dashboard" element={<Dashboard />} />
+            <Route path="/pagecontent/billing" element={<Billing />} />
+          </Route>
+        </Route>
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/login" element={<SignIn />} />
       </Routes>
